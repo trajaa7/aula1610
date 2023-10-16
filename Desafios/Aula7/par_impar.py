e67061c5b438cdde558a1f3477b8562d546678f8
Giovanalupo <130668804+Giovanalupo@users.noreply.github.com>
2023-10-16 08:52:51 -0300

    Primeiro commit

---
+++ Desafios/Aula7/par_impar.py
@@ -0,0 +1,12 @@
+# Sistema que verifica se o número informado é par ou impar
+
+# Entrada
+
+num = int(input('Digite um número: '))
+
+# Processamento e Saída
+
+if num % 2 == 0:
+    print('Este número é Par!')
+else:
+    print('Este número é impar!')
